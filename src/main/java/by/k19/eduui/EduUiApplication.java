80e51af2895d00221243fcf352d9893afb40f3b8
k-19 <sergeifilinovich01@gmail.com>
2022-09-10 00:32:11 +0300

    Initial commit

---
+++ src/main/java/by/k19/eduui/EduUiApplication.java
@@ -0,0 +1,13 @@
+package by.k19.eduui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduUiApplication.class, args);
+	}
+
+}
